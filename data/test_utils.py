2481712b5cd0961a1365ec56732baa77a2a1afc1
Sun Jin Kim <sk2521@ic.ac.uk>
2022-07-20 14:48:06 +0100

    copy paste code for test_include_coboundary_links

--- data/test_utils.py
+++ data/test_utils.py
@@ -631,3 +631,111 @@ def test_simp_complex_conversion_completes():
 def test_cell_complex_conversion_completes():
     graphs = list(map(convert_to_graph, get_testing_complex_list()))
     _ = convert_graph_dataset_with_rings(graphs, init_rings=True)
+
+
+def test_include_coboundary_links():
+    # copied from test_ring_2complex_dataset_conversion
+    house1 = Data(edge_index=house_edge_index, x=torch.arange(0, 5, dtype=torch.float).view(5, 1), y=torch.tensor([1]))
+    house2 = Data(edge_index=house_edge_index, x=torch.arange(0, 5, dtype=torch.float).view(5, 1), y=torch.tensor([1]))
+    house3 = Data(edge_index=house_edge_index, x=torch.arange(0, 5, dtype=torch.float).view(5, 1), y=torch.tensor([1]))
+    dataset = [house1, house2, house3]
+    complexes, dim, num_features = convert_graph_dataset_with_rings(dataset, include_down_adj=True,
+        init_rings=True)
+    assert dim == 2
+    assert len(num_features) == 3
+    for i in range(len(num_features)):
+        assert num_features[i] == 1
+    assert len(complexes) == 3
+    for i in range(len(complexes)):
+        # Do some basic checks for each complex.
+        # Checks the number of rings in `boundary_index`
+        assert complexes[i].cochains[2].boundary_index[:, 1].max().item() == 1
+        assert complexes[i].dimension == 2
+        assert complexes[i].nodes.boundary_index is None
+        assert list(complexes[i].edges.boundary_index.size()) == [2, 2*6]
+        assert list(complexes[i].two_cells.boundary_index.size()) == [2, 3+4]
+        assert complexes[i].edges.lower_index.size(1) == 18
+        assert torch.equal(complexes[i].nodes.x, house1.x)
+        assert torch.equal(complexes[i].y, house1.y)
+    # these checks are ones that I added on top of test_ring_2complex_dataset_conversion
+
+
+
+    
+    # copied from test_construction_of_ring_2complex
+    house = Data(edge_index=house_edge_index, x=torch.arange(0, 5, dtype=torch.float).view(5, 1), y=torch.tensor([1]))
+    house.num_nodes = house_edge_index.max().item() + 1
+
+    house_complex = compute_ring_2complex(house.x, house.edge_index, None, house.num_nodes,
+                                          max_k=4, y=house.y, init_rings=True)
+
+    # Check the number of cells
+    assert house_complex.nodes.num_cells_down is None
+    assert house_complex.nodes.num_cells_up == 6
+    assert house_complex.nodes.boundary_index is None
+    assert house_complex.edges.num_cells_down == 5
+    assert house_complex.edges.num_cells_up == 2
+    assert list(house_complex.edges.boundary_index.size()) == [2, 2*6]
+    assert house_complex.cochains[2].num_cells == 2
+    assert house_complex.cochains[2].num_cells_down == 6
+    assert house_complex.cochains[2].num_cells_up == 0
+    assert list(house_complex.cochains[2].boundary_index.size()) == [2, 3+4]
+
+    # Check the returned parameters
+    v_params = house_complex.get_cochain_params(dim=0)
+    assert torch.equal(v_params.x, house.x)
+    assert v_params.down_index is None
+
+    expected_v_up_index = torch.tensor([[0, 1, 0, 3, 1, 2, 2, 3, 2, 4, 3, 4],
+                                        [1, 0, 3, 0, 2, 1, 3, 2, 4, 2, 4, 3]], dtype=torch.long)
+    assert torch.equal(v_params.up_index, expected_v_up_index)
+
+    expected_v_up_attr = torch.tensor([[1], [1], [3], [3], [3], [3],
+                                       [5], [5], [6], [6], [7], [7]], dtype=torch.float)
+    assert torch.equal(v_params.kwargs['up_attr'], expected_v_up_attr)
+    assert v_params.kwargs['down_attr'] is None
+    assert v_params.kwargs['boundary_attr'] is None
+
+    e_params = house_complex.get_cochain_params(dim=1)
+    expected_e_x = torch.tensor([[1], [3], [3], [5], [6], [7]], dtype=torch.float)
+    assert torch.equal(e_params.x, expected_e_x)
+
+    expected_e_up_index = torch.tensor([[0, 1, 0, 2, 0, 3, 1, 2, 1, 3, 2, 3, 3, 4, 3, 5, 4, 5],
+                                        [1, 0, 2, 0, 3, 0, 2, 1, 3, 1, 3, 2, 4, 3, 5, 3, 5, 4]], dtype=torch.long)
+    assert torch.equal(e_params.up_index, expected_e_up_index)
+
+    expected_e_up_attr = torch.tensor([[6], [6], [6], [6], [6], [6], [6], [6], [6], [6], [6], [6], [9], [9], [9], [9], [9], [9]], dtype=torch.float)
+    assert torch.equal(e_params.kwargs['up_attr'], expected_e_up_attr)
+
+    expected_e_down_index = torch.tensor([[0, 1, 0, 2, 2, 3, 2, 4, 3, 4, 1, 3, 1, 5, 3, 5, 4, 5],
+                                          [1, 0, 2, 0, 3, 2, 4, 2, 4, 3, 3, 1, 5, 1, 5, 3, 5, 4]],
+                                         dtype=torch.long)
+    assert torch.equal(e_params.down_index, expected_e_down_index)
+    expected_e_down_attr = torch.tensor([[0], [0], [1], [1], [2], [2], [2], [2], [2], [2],
+                                         [3], [3], [3], [3], [3], [3], [4], [4]],
+                                        dtype=torch.float)
+    assert torch.equal(e_params.kwargs['down_attr'], expected_e_down_attr)
+
+    assert torch.equal(e_params.kwargs['boundary_attr'], house.x)
+    assert list(e_params.kwargs['boundary_index'].size()) == [2, 2*house_complex.edges.num_cells]
+    assert torch.equal(e_params.kwargs['boundary_index'][1], torch.LongTensor([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]))
+    assert torch.equal(e_params.kwargs['boundary_index'][0], torch.LongTensor([0, 1, 0, 3, 1, 2, 2, 3, 2, 4, 3, 4]))
+
+    t_params = house_complex.get_cochain_params(dim=2)
+    expected_t_x = torch.tensor([[6], [9]], dtype=torch.float)
+    assert torch.equal(t_params.x, expected_t_x)
+    expected_t_down_index = torch.tensor([[0, 1],
+                                          [1, 0]],
+                                         dtype=torch.long)
+    assert torch.equal(t_params.down_index, expected_t_down_index)
+    expected_t_down_attr = torch.tensor([[5], [5]], dtype=torch.float)
+    assert torch.equal(t_params.kwargs['down_attr'], expected_t_down_attr)
+    
+    assert t_params.up_index is None
+    assert torch.equal(t_params.kwargs['boundary_attr'], expected_e_x)
+    expected_t_boundary_index = torch.tensor([[0, 1, 2, 3, 3, 4, 5],
+                                          [0, 0, 0, 0, 1, 1, 1]], dtype=torch.long)
+    assert torch.equal(t_params.kwargs['boundary_index'], expected_t_boundary_index)
+    assert torch.equal(house_complex.y, house.y)
+
+    # these checks are ones that I added on top of test_construction_of_ring_2complex
