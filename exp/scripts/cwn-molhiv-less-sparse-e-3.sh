6778d47998132bf820565c0a2ba6b8396023737f
sunjink1m <sk2521@ic.ac.uk>
2022-08-06 23:11:11 +0100

    Change num_workers 2->0 in molhiv LR scripts

--- exp/scripts/cwn-molhiv-less-sparse-e-3.sh
+++ exp/scripts/cwn-molhiv-less-sparse-e-3.sh
@@ -22,7 +22,7 @@ python -m exp.run_mol_exp \
   --emb_dim 64 \
   --batch_size 128 \
   --epochs 150 \
-  --num_workers 2 \
+  --num_workers 0 \
   --preproc_jobs 10 \
   --task_type bin_classification \
   --eval_metric ogbg-molhiv \
