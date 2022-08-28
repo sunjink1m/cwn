988089e4db7faba0d3455e7a560218c6a0f58603
sunjink1m <sun.jin.k.1000@gmail.com>
2022-08-29 00:10:32 +0100

    Add molhiv deeper script

---
+++ exp/scripts/cwn-molhiv-deeper-less-sparse.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+python -m exp.run_mol_exp \
+  --device 0 \
+  --start_seed 0 \
+  --stop_seed 9 \
+  --exp_name cwn-molhiv-deeper-less-sparse \
+  --dataset MOLHIV \
+  --model ogb_embed_less_sparse_deeper \
+  --use_coboundaries True \
+  --use_boundaries True \
+  --indrop_rate 0.0 \
+  --drop_rate 0.2 \
+  --res_drop_rate 0.2 \
+  --graph_norm bn \
+  --drop_position lin2 \
+  --nonlinearity relu \
+  --readout mean \
+  --final_readout sum \
+  --lr 0.0001 \
+  --lr_scheduler None \
+  --num_layers 7 \
+  --emb_dim 128 \
+  --batch_size 128 \
+  --epochs 150 \
+  --num_workers 0 \
+  --preproc_jobs 32 \
+  --task_type bin_classification \
+  --eval_metric ogbg-molhiv \
+  --max_dim 2  \
+  --max_ring_size 12 \
+  --init_method sum \
+  --train_eval_period 10 \
+  --use_edge_features \
+  --include_down_adj \
+  --dump_curves
